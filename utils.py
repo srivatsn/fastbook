149a28adac7f83e44f77582002e46b1bae9733fe
Srivatsn Narayanan <srivatsn@users.noreply.github.com>
2022-08-15 01:56:23 +0000

    Updates

--- utils.py
+++ utils.py
@@ -30,14 +30,16 @@ def get_image_files_sorted(path, recurse=True, folders=None): return get_image_f
 from azure.cognitiveservices.search.imagesearch import ImageSearchClient as api
 from msrest.authentication import CognitiveServicesCredentials as auth
 
-def search_images_bing(key, term, min_sz=128, max_images=150):    
-     params = {'q':term, 'count':max_images, 'min_height':min_sz, 'min_width':min_sz}
-     headers = {"Ocp-Apim-Subscription-Key":key}
-     search_url = "https://api.bing.microsoft.com/v7.0/images/search"
-     response = requests.get(search_url, headers=headers, params=params)
-     response.raise_for_status()
-     search_results = response.json()    
-     return L(search_results['value'])
+
+def search_images_bing(key, term, min_sz=128, max_images=150, offset=0):
+    params = {'q': term, 'count': max_images, 'offset': offset,
+              'min_height': min_sz, 'min_width': min_sz}
+    headers = {"Ocp-Apim-Subscription-Key": key}
+    search_url = "https://api.bing.microsoft.com/v7.0/images/search"
+    response = requests.get(search_url, headers=headers, params=params)
+    response.raise_for_status()
+    search_results = response.json()
+    return L(search_results['value'])
 
 
 # -
